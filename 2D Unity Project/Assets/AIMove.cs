94d6bc3b49257be5a3fc69ab8e0f364794365878
Ewan Withey <516806@chrome.escg.ac.uk>
2020-10-12 12:48:56 +0100

    Added a second sprite

---
+++ 2D Unity Project/Assets/AIMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIMove : MonoBehaviour
+{
+
+    float timer;
+    float xSpeed;
+    float xDirection = 2;
+
+    void Start()
+    {
+        timer = 0;
+        xSpeed = 0f;
+    }
+
+    void Update()
+    {
+        timer++;
+        Debug.Log(timer);
+     
+        if (timer > 75)
+        {
+            timer = 0f;
+            xDirection = xDirection - xDirection;            
+            xSpeed = 0.5f;
+            transform.rotation = Quaternion.Euler(0, 180f, 0);
+            transform.position = new Vector3(xDirection, 3, 0);
+        }
+
+    }
+}
+
